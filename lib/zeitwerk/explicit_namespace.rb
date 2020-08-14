d04d7a6aece683f87d6a597a1a6f6c9f4fb214b4
Jean Boussier <jean.boussier@gmail.com>
2020-08-14 17:14:52 +0200

    Keep a reference on TracePoint#self

--- lib/zeitwerk/explicit_namespace.rb
+++ lib/zeitwerk/explicit_namespace.rb
@@ -56,15 +56,16 @@ module Zeitwerk
       end
 
       def tracepoint_class_callback(event)
+        mod = event.self
         # If the class is a singleton class, we won't do anything with it so we
         # can bail out immediately. This is several orders of magnitude faster
         # than accessing its name.
-        return if event.self.singleton_class?
+        return if mod.singleton_class?
 
         # Note that it makes sense to compute the hash code unconditionally,
         # because the trace point is disabled if cpaths is empty.
-        if loader = cpaths.delete(real_mod_name(event.self))
-          loader.on_namespace_loaded(event.self)
+        if loader = cpaths.delete(real_mod_name(mod))
+          loader.on_namespace_loaded(mod)
           disable_tracer_if_unneeded
         end
       end
